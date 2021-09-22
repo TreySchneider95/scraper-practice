e9f861ad2f623298d0587b53a00750705bb72c40
TreySchneider95 <tschneid95@gmail.com>
2021-09-22 12:46:57 -0700

    Modified to work on tableful

--- quote_scraper.py
+++ quote_scraper.py
@@ -20,25 +20,37 @@ try:
 
     # While next button is found in the page the loop runs
     while True:
-        # Loops through quote in the page
-        for quote in bs.findAll('div', {'class': 'quote'}):
-            # Extract the text part of quote, author and tags
-            text = quote.find('span', {'class': 'text'}).text
-            author = quote.find('small', {'class': 'author'}).text
+        dict = {}
+        tag_counter = 0
+        quote_counter = 0
+        for foo in bs.findAll('td', {'style': 'padding-bottom: 2em;'}):
             tags = []
-            for tag in quote.findAll('a', {'class': 'tag'}):
+            for tag in foo.findAll('a'):
                 tags.append(tag.text)
+            dict[tag_counter]=tags
+            tag_counter += 1
+        # Loops through quote in the page
+        for quote in bs.findAll('tr', {'style': 'border-bottom: 0px; '}):
+            # Extract the text part of quote, author and tags
+            text = quote.find('td', {'style': 'padding-top: 2em;'}).text
+            lst = text.split(' Author: ')
+            text = lst[0]
+            author = lst[1]
+            # print(7)
+            # print(9)
             # Writes the current quote,author and tags to a csv file
             dictwriter.writerow(
-                {'quote': text, 'author': author, 'tags': tags})
+                {'quote': text, 'author': author, 'tags': dict[quote_counter]})
+            quote_counter += 1
+                # {'quote': text, 'author': author})
 
         # Finds the link to next page
-        next = bs.find('li', {'class': 'next'})
-        if not next:
+        next = [(x.text, x['href']) for x in bs.find_all('a')if 'Next \n                    ' in x.text]
+        if len(next) == 0:
             break
 
         # Gets and parses the html file of next page
-        html = requests.get(URL+next.a.attrs['href'])
+        html = requests.get(URL+next[0][1][9:])
         bs = BeautifulSoup(html.text, 'html.parser')
 except:
     print('Unknown Error!!!')
